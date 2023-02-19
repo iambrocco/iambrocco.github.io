0be7b3f269b9e663cb1792885671b884cee3df0d
IamBrocco <iambrocco@programmer.net>
2023-02-19 14:09:28 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+var dc = document.getElementById("discordstuff");
+var mc = document.getElementById("minecraftstuff");
+var gen = document.getElementById("generationstuff");
+var brocco = document.getElementById("brocco");
+
+mc.onclick = () => {
+    window.location.href = "minecraft/"
+}
+dc.onclick = () => {
+    window.location.href = "discord/"
+}
+gen.onclick = () => {
+    window.location.href = "generators/"
+}
+brocco.onclick = () => {
+    window.location.href = "about/"
+}
